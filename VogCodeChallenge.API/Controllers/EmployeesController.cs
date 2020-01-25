117223b8a9951bcdef94ec68aaee3e56d1f6fa8d
Soji Egbonwon <sojiegbonwon@yahoo.com>
2020-01-25 17:54:25 +0100

    step_4 provide 2 RESTFUL endpoints

---
+++ VogCodeChallenge.API/Controllers/EmployeesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VogCodeChallenge.Core.Interfaces;
+using VogCodeChallenge.Core.Models;
+
+namespace VogCodeChallenge.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeManager _employeeManager;
+
+        public EmployeesController(IEmployeeManager employeeManager)
+        {
+            _employeeManager = employeeManager;
+        }
+
+        // GET api/employees
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            try
+            {
+                var employees = _employeeManager.ListAll();
+                if (employees == null || !employees.Any())
+                {
+                    return NotFound("No record found");
+                }
+                return Ok(employees);
+            }
+            catch (Exception exception)
+            {
+                return NotFound(exception.Message);
+            }
+        }
+
+        // GET api/employees/department/{departmentId}
+        [HttpGet("department/{departmentId}")]
+        public ActionResult<IEnumerable<Employee>> Get(int departmentId)
+        {
+            try
+            {
+                var employees = _employeeManager.GetAll().Where(p => p.Department.ID == departmentId);
+                if (employees == null || !employees.Any())
+                {
+                    return NotFound($"Cannot find records for specified department {departmentId}");
+                }
+                return Ok(employees);
+            }
+            catch (Exception exception)
+            {
+                return NotFound(exception.Message);
+            }
+        }
+    }
+}
