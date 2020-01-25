5664f01bc783ee1fd2451f1eae61b0d79e4529c9
Soji Egbonwon <sojiegbonwon@yahoo.com>
2020-01-26 00:42:37 +0100

    step_7

--- VogCodeChallenge.API/Startup.cs
+++ VogCodeChallenge.API/Startup.cs
@@ -19,9 +19,12 @@ namespace VogCodeChallenge.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IHostingEnvironment _env;
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
@@ -30,9 +33,21 @@ namespace VogCodeChallenge.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+           
+            /*In response to step 7, using Environment variables, we can easily switch from in-memory database to a real Database
+            Irrespective of the source (launchsettings, commandline, OS environment), we can check the applicable environment variable and determine the application logic
+            as shown below. With this implementation, there is no need to update the domain/service implementation we used for data access and proccessing*/
+            if (_env.IsDevelopment()) // use in-memory if running in development mode
+            {
+                services.AddDbContext<VogDBContext>(options => options.UseInMemoryDatabase(databaseName: "VogDB"));
+            }
+            else //use a Database (i.e. SQL Server) for other environment. In fact, we could determine different databases for staging and production
+            {
+                var connectionString = Environment.GetEnvironmentVariable("VogDB_ConnectionString");
+                services.AddDbContext<VogDBContext>(options => options.UseSqlServer(connectionString));
+            }
 
             // bind our interfaces to implementation classes
-            services.AddDbContext<VogDBContext>(options => options.UseInMemoryDatabase(databaseName: "VogDB"));
             services.AddScoped<IEmployeeManager, EmployeeManager>();
         }
 
