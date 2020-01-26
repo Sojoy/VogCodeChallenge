4fca9eb2eeebe58f7db8e490da39864334b19e22
Soji Egbonwon <sojiegbonwon@yahoo.com>
2020-01-26 01:01:47 +0100

    step_8

---
+++ VogCodeChallenge.ConsoleQuestion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VogCodeChallenge.ConsoleQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to test question");
+
+            QuestionClass.TestQuestion();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VogCodeChallenge.ConsoleQuestion/QuestionClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.ConsoleQuestion
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey"
+        };
+
+        public static void TestQuestion()
+        {
+            //the foreach statement is a straightforward and simpler syntax than directly iterating records using Enumerator object on a collection
+            foreach (var name in NamesList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
